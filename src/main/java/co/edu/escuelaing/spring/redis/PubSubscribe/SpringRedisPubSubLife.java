8620d5e2b850e0aa9e5b24a061af4d50e21ebe5c
Juano <juanosierra2@gmail.com>
2022-07-12 10:28:04 -0500

    Ejemplo clase

---
+++ src/main/java/co/edu/escuelaing/spring/redis/PubSubscribe/SpringRedisPubSubLife.java
@@ -0,0 +1,10 @@
+package co.edu.escuelaing.spring.redis.PubSubscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringRedisPubSubLife {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisPubSubLife.class, args);
+    }
+}
